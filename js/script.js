945f910bbd0ca84e4145e5d74bab6c8bed31945e
apospelov13 <small99@hotbox.ru>
2017-05-31 11:46:49 +0300

    v1

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var animation = function(){	
+	var clientScreenH = $(window).height();
+	var scrollTop = $(window).scrollTop();			  	
+	$(".animation").each(function() {
+		var AnimationType = $(this).attr("data-animate");		
+		var AelementT = $(this).offset().top - scrollTop;    
+		$(this).attr("data-top", AelementT + "px");		
+		var thisPosition = AelementT;
+		if ((clientScreenH - 50) > thisPosition){
+			$(this).addClass(AnimationType + " animated");
+		}else {
+			$(this).removeClass(AnimationType + " animated");
+		}		
+	});	
+};
+$(window).load(function(){
+	animation();
+});
+$(window).scroll(function() {
+  animation();
+});
+
+/*navigation*/
+$('.mobileMenu').click(function(e){
+	e.preventDefault();
+	e.stopPropagation();
+	$('header, main').toggleClass('open');
+	$('body').toggleClass('overflow-hedden');
+});
