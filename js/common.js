febc65afee02a8ed1b531a3f7fed5bb48f51895e
apospelov13 <small99@hotbox.ru>
2017-05-31 18:32:27 +0300

    v4

--- js/common.js
+++ js/common.js
@@ -1,2 +1,15 @@
+//Parallax
 $('.parallax-window').parallax({imageSrc: 'img/banner_section_two.png'});
+
+//Animation FadeIn
+$(window).scroll(function() {
+  $(".animation").each(function(){
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 700) {
+      $(".fadeUp").removeClass("fadeUp").addClass("animated fadeInUpBig");
+      $(".fadeDown").removeClass("fadeDown").addClass("animated fadeInDownBig");
+    }
+  });
+});
 
